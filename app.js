c4ba42cd0595b44b26b869ebcb4015b27ecc2eab
mm86 <madhuvanmahesh@gmail.com>
2016-06-23 23:25:56 -0700

    fix: Remove comments
    Remove unnecessary comments.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('input').keydown(function(e) {
     if(e.keyCode === 13) {
-      // do whatever you want with the value
+
       alert($(this).val());
     }
   });
