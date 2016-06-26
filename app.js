357a53ef0d9f4f7ac0ca4004663fd4d5baff456d
mm86 <madhuvanmahesh@gmail.com>
2016-06-25 17:07:37 -0700

    fix: Enable new tab for clicked urls
    When user clicks on an item, a new tab opens displaying the clicked item's wikipedia page.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-    
+
     $('input').keydown(function(e) {
         if (e.keyCode === 13) {
             $("#list ul").empty();
             var input = $(this).val()
-            //make the corresponding API call and console.log the results
-            
-            var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+ input +'&format=json&callback=?';
+                //make the corresponding API call and console.log the results
+
+            var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + input + '&format=json&callback=?';
             $.ajax({
                 url: url,
                 dataType: 'jsonp',
@@ -17,9 +17,22 @@ $(document).ready(function() {
                 var snippet = results[2];
                 var urls = results[3];
 
-                for(var i in title){
-                    $("#list ul").append( "<li><a href="+urls[i]+">"+title[i]+": "+snippet[i]+"</a></li>" )
+                for (var i in title) {
+                    $("#list ul").append("<li><a href=" + urls[i] + ">" + title[i] + ": " + snippet[i] + "</a></li>");
+
                 }
+                
+                $("#list ul a").click(function(e) {
+                    e.preventDefault();
+                    var urlVal = $(this).attr("href");
+                    var newTab = window.open(urlVal, '_blank');
+                    if (newTab) {
+                        newTab.focus();
+                    } else {
+                        alert('enable popups for this website');
+                    }
+                });
+
 
             })
 
